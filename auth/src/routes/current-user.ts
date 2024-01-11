ebc1affacc09d4e5dd1c05b92e0e835a174c8e1e
mune <munerato.ale@gmail.com>
2024-01-11 14:20:53 +0100

    feat: :sparkles: Implemented Auth service as draft

---
+++ auth/src/routes/current-user.ts
@@ -0,0 +1,9 @@
+import express from "express";
+
+const router = express.Router();
+
+router.get("/api/users/currentuser", (req, res) => {
+    res.send({ currentUser: null });
+});
+
+export { router as currentUserRouter };
