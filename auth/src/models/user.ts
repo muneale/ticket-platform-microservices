ae910ff401fb8f6c9f994422ad167ce70235b1d4
mune <munerato.ale@gmail.com>
2024-01-12 12:00:35 +0100

    feat: :sparkles: Implemented user signup logic
    Password hasher and BadRequestError were also been added.

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,4 +1,5 @@
 import { Document, model, Model, Schema } from "mongoose";
+import { Password } from "../services/password";
 
 // An interface that describes the properties
 // that are required to create a new User
@@ -31,6 +32,14 @@ const userSchema = new Schema({
         type: String,
         required: true
     }
+});
+
+userSchema.pre("save", async function (done) {
+    if (this.isModified("password")) {
+        const hashed = await Password.toHash(this.get("password"));
+        this.set("password", hashed);
+    }
+    done();
 });
 
 userSchema.statics.build = (attrs: UserAttrs) => {
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,8 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
 import { RequestValidationError } from "../errors/request-validation-error";
-import { DatabaseConnectionError } from "../errors/database-connection-error";
+import { User } from "../models/user";
+import { BadRequestError } from "../errors/bad-request-error";
 
 const router = express.Router();
 
@@ -21,9 +22,18 @@ router.post("/api/users/signup", [
     }
 
     const { email, password } = req.body;
-    throw new DatabaseConnectionError();    
+    
+    const existingUser = await User.findOne({ email });
 
-    res.send({});
+    if (existingUser) {
+        throw new BadRequestError("Email in use");
+    }
+
+    const user = User.build({ email, password });
+    await user.save();
+
+    console.log("User created");
+    res.status(201).send(user);
 });
 
 export { router as signupRouter };
