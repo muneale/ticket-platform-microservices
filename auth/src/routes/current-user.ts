dd8792d74bb335fc96b140c3f76bcca26c02aab2
mune <munerato.ale@gmail.com>
2024-01-13 16:57:28 +0100

    feat: :sparkles: Add currentuser API

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,23 @@
 import express from "express";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
 router.get("/api/users/currentuser", (req, res) => {
-    res.send({ currentUser: null });
+    const token = req.session?.jwt;
+    
+    // If there is no token, send back null
+    if (!token) {
+        return res.send({ currentUser: null });
+    }
+
+    // If there is a token, verify it
+    try {
+        const payload = jwt.verify(token, process.env.JWT_KEY!);
+        res.send({ currentUser: payload });
+    } catch (error) {
+        res.send({ currentUser: null });
+    }
 });
 
 export { router as currentUserRouter };
