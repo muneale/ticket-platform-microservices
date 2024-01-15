b57b9610d631ff5ba8073cf3125e543129b7183b
mune <munerato.ale@gmail.com>
2024-01-15 10:16:06 +0100

    feat: :sparkles: CurrentUser and RequireAuth middlewares are been implemented

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,23 +1,14 @@
 import express from "express";
-import jwt from "jsonwebtoken";
+import { currentUser } from "../middlewares/current-user";
 
 const router = express.Router();
 
-router.get("/api/users/currentuser", (req, res) => {
-    const token = req.session?.jwt;
-    
-    // If there is no token, send back null
-    if (!token) {
-        return res.send({ currentUser: null });
+router.get(
+    "/api/users/currentuser",
+    currentUser,
+    (req, res) => {
+        res.send({ currentUser: req.currentUser || null });
     }
-
-    // If there is a token, verify it
-    try {
-        const payload = jwt.verify(token, process.env.JWT_KEY!);
-        res.send({ currentUser: payload });
-    } catch (error) {
-        res.send({ currentUser: null });
-    }
-});
+);
 
 export { router as currentUserRouter };
