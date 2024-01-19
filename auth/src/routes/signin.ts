051c1c9e649e5120c07807b0a5c3e2b16a8efaeb
mune <munerato.ale@gmail.com>
2024-01-19 17:38:43 +0100

    refactor: :recycle: Moved errors and middlewares inside a common npm library

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -2,9 +2,8 @@ import express, { Request, Response } from "express";
 import { body } from "express-validator";
 import jwt from "jsonwebtoken";
 
-import { validateRequest } from "../middlewares/validate-request";
+import { validateRequest, BadRequestError } from "@water3829-tickets/common";
 import { User } from "../models/user";
-import { BadRequestError } from "../errors/bad-request-error";
 import { PasswordManager } from "../services/password-manager";
 
 const router = express.Router();
